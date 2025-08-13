f297240f155819dd75d8376e733850647084b773
3788-lacarrillo6 <118786400+lacarrillo2001@users.noreply.github.com>
2025-08-12 23:43:17 -0500

    a

--- Frontend/examen-front/src/types/grupo.ts
+++
@@ -1,8 +0,0 @@
-export interface Grupo {
-  id: number;
-  linea_id: number;
-  disponibilidad_id: number;
-  miembros: number;
-  creado_en: string; // formato ISO desde PostgreSQL
-  actualizado_en: string; // formato ISO
-}
--- Frontend/examen-front/src/types/lineaInvestigacio.ts
+++
@@ -1,5 +0,0 @@
-export interface LineaInvestigacion {
-  id: number;
-  nombre: string;
-  area: string;
-}
